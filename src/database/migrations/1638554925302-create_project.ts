2cbb33a050dfb9a42ee97476de6bb253d693fc5d
jonasdevzero <jonasdevzero@gmail.com>
2021-12-05 12:21:26 -0300

    Adding Skill features:
    - Added skill controller methods
    - Fix create_project migration query error

--- src/database/migrations/1638554925302-create_project.ts
+++ src/database/migrations/1638554925302-create_project.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class createProject1638554925302 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'");
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
         await queryRunner.createTable(new Table({
             name: "project",
             columns: [
