4a58527c2c1f32832279c2bf86422d53db006d2c
jonasdevzero <jonasdevzero@gmail.com>
2021-12-14 21:59:28 -0300

    adding cluster

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,41 @@
-import "dotenv"
-import "reflect-metadata"
-import "./database/connection"
-import fastify from "fastify"
-import fastifyCors from "fastify-cors"
-import fastifyJWT from "fastify-jwt"
-import routes from "./routes"
+import "dotenv";
+import "./database/connection";
+import fastify from "fastify";
+import fastifyCors from "fastify-cors";
+import fastifyJWT from "fastify-jwt";
+import cluster from "cluster";
+import { cpus } from "os";
+import routes from "./routes";
 
-const port = process.env.PORT || 5000
-const host = "0.0.0.0"
-const secret = process.env.ADMIN_SECRET || "devzero"
+const port = process.env.PORT || 5000;
+const host = "0.0.0.0";
+const secret = process.env.ADMIN_SECRET || "devzero";
 
-const server = fastify({ logger: process.env.ENVIRONMENT === "development" })
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
+  }
 
-server.register(fastifyCors, { origin: "*", methods: ["GET, POST", "PUT", "DELETE", "PATCH"] })
-server.register(fastifyJWT, { secret })
-server.register(routes)
+  cluster.on("exit", (worker) => {
+    console.log(`Process ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const server = fastify({ logger: process.env.ENVIRONMENT === "development" });
 
-server.listen(port, host, async (err, address) => {
+  server.register(fastifyCors, {
+    origin: "*",
+    methods: ["GET, POST", "PUT", "DELETE", "PATCH"],
+  });
+  server.register(fastifyJWT, { secret });
+  server.register(routes);
+
+  server.listen(port, host, async (err, address) => {
     if (err) {
-        server.log.error(err)
-        process.exit(1)
+      server.log.error(err);
+      process.exit(1);
     }
 
-    server.log.info(`server listening on ${address}`)
-})
+    server.log.info(`server listening on ${address}`);
+  });
+}
